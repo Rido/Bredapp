065fd2da520f4f2319e4cf8c873fc5d439b52d0e
rhamoudi <r.hamoudi@hotmail.com>
2012-11-14 12:40:50 +0100

    Fix activity post

---
+++ Bredapp/TempActivity.h
@@ -0,0 +1,13 @@
+//
+//  TempActivity.h
+//  Bredapp
+//
+//  Created by atmstudent on 11/14/12.
+//  Copyright (c) 2012 Avans_Groep2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TempActivity : NSObject
+
+@end
---
+++ Bredapp/TempActivity.m
@@ -0,0 +1,13 @@
+//
+//  TempActivity.m
+//  Bredapp
+//
+//  Created by atmstudent on 11/14/12.
+//  Copyright (c) 2012 Avans_Groep2. All rights reserved.
+//
+
+#import "TempActivity.h"
+
+@implementation TempActivity
+
+@end
